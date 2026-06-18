46f8997eccab130be479ee3b1605657a6119f7f7
Crescencio Corpas <ccorpasmena@gmail.com>
2026-06-18 12:55:08 +0200

    instruccion cargo

---
+++ modulo01-cargo/src/main.rs
@@ -0,0 +1,9 @@
+fn main() 
+{
+    println!("La instrucción CARGO");
+    println!("1. Para crear un nuevo proyecto escribimos: cargo new nombre_del proyecto");
+    println!("2. Para arrancar el programa escribimos en la terminal: cargo run");
+    println!("3. La instrucción cargo build solo compila el código pero NO lo ejecuta.");
+    println!("4. Para no tener que formatear el código podemos escribir: cargo fmt");
+    println!("5. Para mostrar sugerencias sobre la sintaxis del código podemos escribir: cargo clippy");
+}
