b1cf69bd3793fa34ab5053bdeac51402cb6d73c0
Crescencio Corpas <ccorpasmena@gmail.com>
2026-06-24 13:48:37 +0200

    tuplas y arrays

--- modulo2-fundamentos/src/main.rs
+++ modulo2-fundamentos/src/main.rs
@@ -1,16 +1,19 @@
 fn main() 
 {
-    println!("Fundamentos: variables, mutabilidad e inmutabilidad, sombreado, tipos de datos y operadores");
-    println!();
-
+    // Fundamentos: variables, tipos de datos (primitivos y compuestos), operadores, mutabilidad e inmutabilidad y sombreado.
+    
     // Para nombrar las variables usamos la nomenclatura snake_case
     let mi_variable = 8;
     println!("La variable vale: {mi_variable}");
     println!();
 
-    // Tipos de datos
-    let edad = 18;   // Número entero
-    println!("Mi edad es: {edad}");
+    // Tipos de datos primitivos
+    let entero_sin_signo: u32 = 5;   // Número siempre positivo
+    println!("El número es: {entero_sin_signo}");
+    println!();
+
+    let entero_con_signo = -18;   // Número entero positivo o negativo
+    println!("El número es: {entero_con_signo}");
     println!();
 
     let precio_lata = 5.25;   // Número flotante
@@ -24,6 +27,15 @@ fn main()
     let letra_dni = 'D';   // Character
     println!("La letra de mi DNI es: {letra_dni}");
     println!();
+
+    // Tipos de datos compuestos
+    let mi_tupla= (-47, 74.5, true, "Pepe", 'w');   // Tuplas
+    println!("El valor según el índice introducido es: {}", mi_tupla.3);   // Se accede a los valores a través del índice con un punto
+    println!();
+
+    let mi_array = ["Crecen", "Mónica", "Manuel", "Adrián"];   // Arrays
+    println!("El valor según el índice introducido es: {}", mi_array[1]);   // Se accede a los valores a través del índice con corchetes
+    println!();    
     
     // Mutabilidad e inmutabilidad   
     let mut x = 10;
