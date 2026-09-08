d7743735ebd4b96e7b6bfc305e3699d51d155d2e
Crescencio Corpas <ccorpasmena@gmail.com>
2026-09-08 12:11:13 +0200

    ejercicio de intercambio de valores

---
+++ modulo2-repaso3/src/main.rs
@@ -0,0 +1,18 @@
+/* Ejercicio 3: Intercambio de valores
+
+Declara dos variables: x = 5 e y = 10.
+
+Intercambia sus valores sin usar una tercera variable temporal (pista: piensa en operaciones aritméticas). 
+
+Al final, x debe valer 10 e y debe valer 5. */
+
+fn main() {
+    let x = 5;
+    let y = 10;
+
+    let x = x * 2;
+    println!("La x vale: {x}");
+    let y = y / 2;
+    println!("La y vale: {y}");
+    
+}
