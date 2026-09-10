85364dbe425e3f59e0b43f270c08625760dd6703
Crescencio Corpas <ccorpasmena@gmail.com>
2026-09-10 19:29:26 +0200

    mi primera struct

---
+++ modulo4-repaso3/src/main.rs
@@ -0,0 +1,17 @@
+/* EJERCICIO 3: Tu primera struct
+
+Define una struct llamada Producto con estos campos:
+
+codigo: u32
+precio: f64
+stock: u32
+en_venta: bool
+
+En main, instancia dos productos distintos con valores inventados.
+
+Imprime en pantalla cada uno de los campos del primer producto, accediendo a ellos con la notación de punto.
+
+Calcula e imprime el valor total del inventario del segundo producto (precio por stock). */
+fn main() {    
+
+}
