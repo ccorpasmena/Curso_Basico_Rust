578b78d85d6277d5bea858bda5e70411c5fbb408
Crescencio Corpas <ccorpasmena@gmail.com>
2026-09-11 11:42:05 +0200

    acabado el ejemplo del Result

--- modulo5-result_T,E/src/main.rs
+++ modulo5-result_T,E/src/main.rs
@@ -1,23 +1,33 @@
 fn main() {
-    let resultado_division = dividir(10, 0);
-    
+    let resultado_division = dividir(100, 20);
+
     match resultado_division {
-        Some(valor) => println!("El resultado de la división es: {valor}"),
-        None => println!("No se puede dividir por cero")
+        Ok(resultado) => println!("Operación completada. El resultado es: {resultado}"),
+        Err(error) => match error {
+            DividirError::DividirPorCero => println!("No se puede dividir entre cero."),
+            DividirError::DivisorInvalido(divisor) => {
+                println!("No se puede dividir entre el número {divisor}")
+            }
+        },
     };
 }
 
 enum DividirError {
     DividirPorCero,
+    DivisorInvalido(i32),
 }
 
 // Option<i32> significa que si existe un valor, va a ser un i32
-fn dividir(dividendo: i32, divisor: i32) -> Result<i32, String>{
-    
+fn dividir(dividendo: i32, divisor: i32) -> Result<i32, DividirError> {
     if divisor == 0 {
         return Err(DividirError::DividirPorCero);
     }
-    
-    let resultado = dividendo/divisor;
+
+    // Por reglas de negocio, por ejemplo no se puede dividir entre 10
+    if divisor == 10 {
+        return Err(DividirError::DivisorInvalido(divisor));
+    }
+
+    let resultado = dividendo / divisor;
     Ok(resultado)
 }
